d018b6829aef2e9a997c67ebad4e12f1db8c7ad5
潘若冰 <han@hantekiMacBook-Pro.local>
2016-11-28 19:49:37 +0800

    Initial Commit

---
+++ ma/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ma
+//
+//  Created by 潘若冰 on 2016/11/28.
+//  Copyright © 2016年 潘若冰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
